35919c85b080afdb6e22aa93290f0d599b951fdd
hamdiboyraz <hamdiboyraz19@gmail.com>
2024-06-30 01:46:04 +0300

    Add an example of using useRef with eventListeners

--- src/App.js
+++ src/App.js
@@ -173,9 +173,20 @@ function Search({ query, setQuery }) {
   const inputEl = useRef(null);
 
   useEffect(function () {
-    console.log(inputEl.current)
-    inputEl.current.focus();
-  }, []);
+    function callback(e) {
+      if (document.activeElement === inputEl.current) return;
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    }
+
+    document.addEventListener("keydown", callback);
+
+    return () => {
+      document.removeEventListener("keydown", callback);
+    }
+  }, [setQuery]);
 
   return (
     <input
