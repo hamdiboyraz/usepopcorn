50547e872fb542337ad7b5eb607807db1de1db28
hamdiboyraz <hamdiboyraz19@gmail.com>
2024-06-30 12:25:55 +0300

    Add countRatingDecisions on movie object by using useRef

--- src/App.js
+++ src/App.js
@@ -249,6 +249,14 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState("");
   const isWatched = watched.map((movie) => movie.imdbId).includes(selectedId);
+
+  const countRef = useRef(0);
+
+  useEffect(function () {
+    if (userRating) countRef.current++;
+    //console.log(countRef.current);
+  }, [userRating]);
+
   const watchedUserRating = watched.find(
     (movie) => movie.imdbId === selectedId
   )?.userRating;
@@ -281,6 +289,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       imdbRating: Number(imdbRating),
       runtime: Number(runtime.split(" ").at(0)),
       userRating,
+      countRatingDecisions: countRef.current,
     };
     onAddWatched(newWatchedMovie);
     onCloseMovie();
